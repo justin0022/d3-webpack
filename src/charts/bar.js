9fbc570978139263c558654c1d1d6fcccd55cda4
Justin Lee <justinlee0022@gmail.com>
2019-01-04 15:44:23 -0800

    Fixed bar chart graph formatting

--- src/charts/bar.js
+++ src/charts/bar.js
@@ -7,12 +7,12 @@ const bar = ({ data, width, height, id }) => {
 
   const x = d3.scaleBand()
     .domain(data.map(d => d.name))
-    .range([margin.left, width - margin.right])
+    .range([margin.left, aWidth - margin.right])
     .padding(0.1)
 
   const y = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.age)]).nice()
-    .range([height - margin.bottom, margin.top])
+    .range([aHeight - margin.bottom, margin.top])
 
   const svg = d3.select(id).append('svg')
     .attr('style', 'display: block;')
@@ -30,13 +30,14 @@ const bar = ({ data, width, height, id }) => {
     .attr('x', d => x(d.name))
     .attr('width', x.bandwidth())
     .attr('y', d => y(d.age))
-    .attr('height', d => height - y(d.age))
+    .attr('height', d => y(0) - y(d.age))
 
   svg.append('g')
-    .attr('transform', `translate(0, ${aHeight})`)
+    .attr('transform', `translate(0, ${aHeight - margin.bottom})`)
     .call(d3.axisBottom(x))
 
   svg.append('g')
+    .attr('transform', `translate(${margin.left},0)`)
     .call(d3.axisLeft(y))
 
   return svg.node()
